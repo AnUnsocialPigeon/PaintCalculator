84da340159d44a5c8ec65445498cc32fc2f01b3c
An Unsocial Pigeon <66815528+AnUnsocialPigeon@users.noreply.github.com>
2021-07-14 10:27:56 +0100

    Add project files.

---
+++ Borwell Software Challenge - Paint Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Borwell_Software_Challenge___Paint_Calculator {
+    class Program {
+        static void Main() {
+            // Program start. GitHub Upload Test.
+        }
+    }
+}
