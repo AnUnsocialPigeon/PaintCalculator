c789b9cc68f9a03ab4995547082872d32d205ac0
An Unsocial Pigeon <66815528+AnUnsocialPigeon@users.noreply.github.com>
2021-07-14 10:54:26 +0100

    First Iteration. Untested.

--- Borwell Software Challenge - Paint Calculator/Program.cs
+++ Borwell Software Challenge - Paint Calculator/Program.cs
@@ -1,13 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/// Author: Jake Turrell
+/// Date: 14/07/2021
+/// Started: 10:28
+/// Finished: N/A
 
+using System;
 namespace Borwell_Software_Challenge___Paint_Calculator {
     class Program {
         static void Main() {
-            // Program start. GitHub Upload Test.
+            // May be attatched to a GUI later.
+            // Hungarian Notation
+
+            // Var Asking.
+            float fWidth = AskFloat("Width: ");
+            float fLength = AskFloat("Length: ");
+            float fHeight = AskFloat("Height: ");
+
+            // Calculations
+            float fFloorArea = fWidth * fLength;
+            float fPaint = 2 * (fWidth + fLength) * fHeight;
+            float fVolume = fWidth * fLength * fHeight;
+
+            // Output
+            Console.WriteLine("\nFloor Area: {0}", fFloorArea);
+            Console.WriteLine("Amount of paint required to paint the walls: {0}", fPaint);
+            Console.WriteLine("Volume of the room: {0}", fVolume);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Prints question to the screen, asks for float
+        /// will do checks.
+        /// </summary>
+        /// <param name="question"></param>
+        /// <returns>user given input as a float</returns>
+        static float AskFloat(string question) {
+            // Var init
+            float fResult;
+            string sAns;
+
+            do {
+                Console.WriteLine(question);
+                sAns = Console.ReadLine();
+
+                // Checks valid number, and if result > 0
+                // (cant have a room with negative or 0 width on wall)
+            } while (float.TryParse(sAns, out fResult) && fResult > 0f); 
+
+            return fResult;
         }
     }
 }
